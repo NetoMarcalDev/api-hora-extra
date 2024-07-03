c678fd1ed7b3f79b4ebba08226efa75140c783f7
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-03 16:39:37 -0300

    Implementação inicial do module, contrller e service.

---
+++ src/hora-extra/hora-extra.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('hora-extra')
+export class HoraExtraController {}
--- src/hora-extra/hora-extra.module.ts
+++ src/hora-extra/hora-extra.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { HoraExtraController } from './hora-extra.controller';
+import { HoraExtraService } from './hora-extra.service';
 
-@Module({})
+@Module({
+  controllers: [HoraExtraController],
+  providers: [HoraExtraService]
+})
 export class HoraExtraModule {}
