6ad3066ac044aa5e239b1a74d63bfcd94f2b0f1b
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-05 18:26:46 -0300

    Injeção da dependência (HoraExtraService) através do construtor no horaExtra.controler.ts.

--- src/hora-extra/hora-extra.controller.ts
+++ src/hora-extra/hora-extra.controller.ts
@@ -1,4 +1,10 @@
+/* eslint-disable prettier/prettier */
 import { Controller } from '@nestjs/common';
+import { HoraExtraService } from './hora-extra.service';
 
 @Controller('hora-extra')
-export class HoraExtraController {}
+export class HoraExtraController {
+    constructor(
+        private horaExtraService: HoraExtraService
+    ){}
+}
