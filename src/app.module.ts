c4997e9a62073e4c8f7115dd14eeba1376d8ea2e
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-05 13:18:15 -0300

    Configuração da conexão Database com TypeORM.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { HoraExtraModule } from './hora-extra/hora-extra.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HoraExtraModule],
+  imports: [
+    HoraExtraModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'horaExtra',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
