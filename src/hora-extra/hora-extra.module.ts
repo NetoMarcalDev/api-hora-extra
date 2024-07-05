c381a7342bfc47759fa81e50359542afc6b25c0c
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-05 13:44:53 -0300

    Implemente o TypeOrmModule.forFeature no imports do hora-extra.module.ts

--- src/hora-extra/hora-extra.module.ts
+++ src/hora-extra/hora-extra.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HoraExtraController } from './hora-extra.controller';
 import { HoraExtraService } from './hora-extra.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HoraExtraRepository } from './repositories/hora-extra.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([HoraExtraRepository])],
   controllers: [HoraExtraController],
-  providers: [HoraExtraService]
+  providers: [HoraExtraService],
 })
 export class HoraExtraModule {}
