0cdfeb52b917f2d50c3dcc84c35bb5052c66deb8
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-05 14:21:27 -0300

    Implementação preparação para a  Persistência de dados.

--- src/hora-extra/hora-extra.service.ts
+++ src/hora-extra/hora-extra.service.ts
@@ -1,4 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HoraExtraRepository } from './repositories/hora-extra.repository';
 
 @Injectable()
-export class HoraExtraService {}
+export class HoraExtraService {
+    constructor(
+        @InjectRepository(HoraExtraRepository)
+        private horaExtraRepository: HoraExtraRepository,
+    ){}
+
+    
+}
