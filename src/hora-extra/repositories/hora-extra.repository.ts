5897144f9222be5740fb7153b0eaa4f871ae06bc
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-05 18:22:20 -0300

    Implementaçã da feature createHoraxtra na horaExtra.repository.ts

--- src/hora-extra/repositories/hora-extra.repository.ts
+++ src/hora-extra/repositories/hora-extra.repository.ts
@@ -1,8 +1,34 @@
 /* eslint-disable prettier/prettier */
 import { Repository, EntityRepository } from 'typeorm';
 import { HoraExtra } from '../entities/hora-extra.entity';
+import { CreateHoraExtraDto } from '../dto/create-hora-extra.dto';
 
 @EntityRepository(HoraExtra)
 export class HoraExtraRepository extends Repository<HoraExtra> {
-    
+    async createHoraExtra( createHoraExtraDto: CreateHoraExtraDto): Promise<HoraExtra> {
+        const {
+            usuario,         
+            estabelecimento,
+            solicitante,
+            data,
+            inicio,
+            fim,
+            total,
+            dia_semana,
+        } = createHoraExtraDto;
+
+        const horaExtra = await this.create({
+            usuario,         
+            estabelecimento,
+            solicitante,
+            data,
+            inicio,
+            fim,
+            total,
+            dia_semana,
+        });
+
+        await this.save(horaExtra);
+        return horaExtra;
+    }
 }
