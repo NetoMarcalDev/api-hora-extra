81e476d039d4959cbf9d87e713a948fff5b69d89
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-05 18:39:32 -0300

    Implementação da feature createHoraExtra no horaExtra.controller.ts.

--- src/hora-extra/hora-extra.controller.ts
+++ src/hora-extra/hora-extra.controller.ts
@@ -1,10 +1,18 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { HoraExtraService } from './hora-extra.service';
+import { CreateHoraExtraDto } from './dto/create-hora-extra.dto';
+import { HoraExtra } from './entities/hora-extra.entity';
 
 @Controller('hora-extra')
 export class HoraExtraController {
     constructor(
         private horaExtraService: HoraExtraService
     ){}
+
+    @Post()
+    createTask(@Body() createHoraExtraDto: CreateHoraExtraDto): Promise<HoraExtra> {
+        return this.horaExtraService.createHoraExtra(createHoraExtraDto);
+    }
+
 }
