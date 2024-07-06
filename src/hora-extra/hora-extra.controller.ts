c5cbfba987a773ad4037e56cd4dbed08f3308f10
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-06 14:44:12 -0300

    Implementação da feature get no controller.

--- src/hora-extra/hora-extra.controller.ts
+++ src/hora-extra/hora-extra.controller.ts
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query,  } from '@nestjs/common';
 import { HoraExtraService } from './hora-extra.service';
 import { CreateHoraExtraDto } from './dto/create-hora-extra.dto';
 import { HoraExtra } from './entities/hora-extra.entity';
+import { GetHoraExtraFiltroDto } from './dto/get-hora-extra-filtro.dto';
 
 @Controller('hora-extra')
 export class HoraExtraController {
@@ -14,5 +15,13 @@ export class HoraExtraController {
     createHoraExtra(@Body() createHoraExtraDto: CreateHoraExtraDto): Promise<HoraExtra> {
         return this.horaExtraService.createHoraExtra(createHoraExtraDto);
     }
+
+    @Get()
+    getHoraExtra(
+        @Query() filtroDto: GetHoraExtraFiltroDto
+      ): Promise<HoraExtra[]> {
+        return this.horaExtraService.getHoraExtra(filtroDto);
+      }
+  
 
 }
