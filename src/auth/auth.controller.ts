68766d70927b09d8417790b5852d7469b643d790
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-06 20:19:23 -0300

    Injeção da depêndencia AuthService no controller.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,8 @@
+/* eslint-disable prettier/prettier */
 import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+}
