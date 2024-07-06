1e7430dbacd58f5534d29c762bb1858e3e722601
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-06 16:52:28 -0300

    Injeção da dependência no repositório do usurário no auth.service.ts.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsuariosRepository } from './repositories/usuario.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(UsuariosRepository)
+        private usuariosRepository: UsuariosRepository
+    ) {}
+}
