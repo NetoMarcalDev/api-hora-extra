c11650bf4cb90aa66ef3274317137778cebbb4f6
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-06 20:23:32 -0300

    Implementação da feature cadastroUsuario no controller.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,15 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
+
+    @Post('/cadastro')
+    cadastroUsuario(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void>{
+        return this.authService.cadastroUsuario(authCredentialsDto);
+    }
+
 }
