3ca477950dbaaa981a861619fef6c471b6af6a3b
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-06 14:38:57 -0300

    Implementação do serviço getHoraExtraFiltroDto.

--- src/hora-extra/hora-extra.service.ts
+++ src/hora-extra/hora-extra.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { HoraExtraRepository } from './repositories/hora-extra.repository';
 import { CreateHoraExtraDto } from './dto/create-hora-extra.dto';
 import { HoraExtra } from './entities/hora-extra.entity';
+import { GetHoraExtraFiltroDto } from './dto/get-hora-extra-filtro.dto';
 
 @Injectable()
 export class HoraExtraService {
@@ -14,6 +15,10 @@ export class HoraExtraService {
 
     createHoraExtra(createHoraExtraDto: CreateHoraExtraDto): Promise<HoraExtra> {
         return this.horaExtraRepository.createHoraExtra(createHoraExtraDto);
+    }
+
+    getHoraExtra(getHoraExtraFiltroDto: GetHoraExtraFiltroDto): Promise<HoraExtra[]> {
+        return this.horaExtraRepository.getHoraExtra(getHoraExtraFiltroDto);
     }
     
 }
