65030f8cfbae376380835f57f442b2bb6745f563
Neto Marçal <neto.marcal.dev@gmail.com>
2024-07-06 22:27:29 -0300

    Implementação implementa as regras de negocio

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from '@nestjs/passport'; 
+import { InjectRepository } from "@nestjs/typeorm";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { JwtPayload } from './jwt-payload.interface';
+import { UsuariosRepository } from "./repositories/usuario.repository";
+import { Usuario } from './entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    
+    constructor(
+        @InjectRepository(UsuariosRepository)
+        private usuariosRepository: UsuariosRepository,
+    ) {
+        super({
+            secretOrKey: '@TopSecret51',
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        });
+    }
+
+    async validate(payload: JwtPayload): Promise<Usuario> {
+        const { descricao } = payload;
+        const usuario: Usuario = await this.usuariosRepository.findOne({descricao });
+
+        if(!usuario) {
+            throw new UnauthorizedException();
+        }
+        return usuario;
+    }
+}
