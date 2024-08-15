3b601e0447980132660595aeb5401ae28f2fbb3b
Neto Marçal <neto.marcal.dev@gmail.com>
2024-08-15 19:56:32 -0300

    Implementações para o gerenciamento de configurações.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { HoraExtraModule } from './hora-extra/hora-extra.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${ process.env.STAGE }`],
+    }),
     HoraExtraModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
