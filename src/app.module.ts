3e1e55f05c2513869a02c8a03ffad1d7e613d890
Neto Marçal <neto.marcal.dev@gmail.com>
2024-08-16 18:49:45 -0300

    Configurações com TypeORM.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { HoraExtraModule } from './hora-extra/hora-extra.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -10,15 +11,19 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: [`.env.stage.${ process.env.STAGE }`],
     }),
     HoraExtraModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '572600',
-      database: 'horaExtra',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     AuthModule,
   ],
