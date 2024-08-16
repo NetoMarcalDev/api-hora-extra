aa487c4e257aea7900faa401b193cdbe3b80b32b
Neto Marçal <neto.marcal.dev@gmail.com>
2024-08-16 19:09:58 -0300

    Validação de esquema de configuração

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { HoraExtraModule } from './hora-extra/hora-extra.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${ process.env.STAGE }`],
+      validationSchema: configValidationSchema,
     }),
     HoraExtraModule,
     TypeOrmModule.forRootAsync({
